78089dfc30c0666e244c872174f77e1b5df2a4f7
Daniel Egger <daniel.egger@axiros.com>
2018-12-28 09:10:02 +0100

    Added test case for an OnBoardRequest and simplified tests
    Signed-off-by: Daniel Egger <daniel.egger@axiros.com>

--- tests/parsing_test.rs
+++ tests/parsing_test.rs
@@ -1,7 +1,75 @@
 mod tests {
     use quick_protobuf::{BytesReader, MessageRead};
-    use rusp::usp::Msg;
-    use rusp::usp_record::Record;
+    use rusp::usp::{mod_Body::OneOfmsg_body::request, Msg};
+    use rusp::usp_record::{mod_Record::OneOfrecord_type::no_session_context, Record};
+
+    #[test]
+    fn simple_notify() {
+        let bytes: Vec<u8> = vec![
+            0x0a, 0x03, 0x31, 0x2e, 0x30, 0x1a, 0x23, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x3a, 0x3a,
+            0x61, 0x78, 0x2d, 0x75, 0x73, 0x70, 0x2d, 0x61, 0x67, 0x65, 0x6e, 0x74, 0x2d, 0x6e,
+            0x6f, 0x73, 0x73, 0x6c, 0x2d, 0x77, 0x65, 0x62, 0x73, 0x6f, 0x63, 0x6b, 0x65, 0x74,
+            0x3a, 0x4d, 0x12, 0x4b, 0x0a, 0x08, 0x0a, 0x04, 0x74, 0x65, 0x73, 0x74, 0x10, 0x03,
+            0x12, 0x3f, 0x0a, 0x3d, 0x42, 0x3b, 0x0a, 0x0f, 0x73, 0x75, 0x62, 0x73, 0x63, 0x72,
+            0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x69, 0x64, 0x42, 0x28, 0x0a, 0x03, 0x6f,
+            0x75, 0x69, 0x12, 0x0d, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x5f, 0x63, 0x6c,
+            0x61, 0x73, 0x73, 0x1a, 0x0d, 0x73, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x5f, 0x6e, 0x75,
+            0x6d, 0x62, 0x65, 0x72, 0x22, 0x03, 0x31, 0x2e, 0x30,
+        ];
+
+        use rusp::usp::{
+            mod_Header::MsgType::NOTIFY, mod_Notify::OneOfnotification::on_board_req,
+            mod_Request::OneOfreq_type::notify,
+        };
+
+        let mut reader = BytesReader::from_bytes(&bytes);
+        let record = Record::from_reader(&mut reader, &bytes).expect("Cannot read Record");
+
+        assert!(record.version == Some("1.0".into()));
+        assert!(record.from_id == Some("proto::ax-usp-agent-nossl-websocket".into()));
+        assert!(record.payload_security == None);
+        assert!(record.mac_signature == None);
+        assert!(record.sender_cert == None);
+        assert!(if let no_session_context(_) = record.record_type {
+            true
+        } else {
+            false
+        });
+
+        if let no_session_context(context) = record.record_type {
+            if let Some(payload) = context.payload {
+                let mut reader = BytesReader::from_bytes(&payload);
+                let msg = Msg::from_reader(&mut reader, &payload).expect("Cannot read Msg");
+                assert!(msg.header.is_some());
+
+                if let Some(header) = msg.header {
+                    assert!(header.msg_id == Some("test".into()));
+                    assert!(header.msg_type == Some(NOTIFY));
+                }
+
+                if let Some(body) = msg.body {
+                    if let request(req) = body.msg_body {
+                        assert!(if let notify(_) = req.req_type {
+                            true
+                        } else {
+                            false
+                        });
+
+                        if let notify(a) = req.req_type {
+                            assert!(a.subscription_id == Some("subscription_id".into()));
+                            assert!(a.send_resp == None);
+                            if let on_board_req(n) = a.notification {
+                                assert!(n.oui == Some("oui".into()));
+                                assert!(n.product_class == Some("product_class".into()));
+                                assert!(n.serial_number == Some("serial_number".into()));
+                                assert!(n.agent_supported_protocol_versions == Some("1.0".into()));
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
 
     #[test]
     fn create_obj() {
@@ -22,17 +90,14 @@ mod tests {
             0x73, 0x74, 0x18, 0x01,
         ];
 
-        use rusp::usp::mod_Body::OneOfmsg_body::request;
-        use rusp::usp::mod_Header::MsgType::ADD;
-        use rusp::usp::mod_Request::OneOfreq_type::add;
-        use rusp::usp_record::mod_Record::OneOfrecord_type::no_session_context;
+        use rusp::usp::{mod_Header::MsgType::ADD, mod_Request::OneOfreq_type::add};
 
         let mut reader = BytesReader::from_bytes(&bytes);
         let record = Record::from_reader(&mut reader, &bytes).expect("Cannot read Record");
 
-        assert!(record.version.clone().unwrap() == "1.0");
-        assert!(record.to_id.clone().unwrap() == "proto::ax-usp-agent-nossl-websocket");
-        assert!(record.from_id.clone().unwrap() == "proto::ax-usp-controller-nossl");
+        assert!(record.version == Some("1.0".into()));
+        assert!(record.to_id == Some("proto::ax-usp-agent-nossl-websocket".into()));
+        assert!(record.from_id == Some("proto::ax-usp-controller-nossl".into()));
         assert!(record.payload_security == None);
         assert!(record.mac_signature == None);
         assert!(record.sender_cert == None);
@@ -49,7 +114,7 @@ mod tests {
                 assert!(msg.header.is_some());
 
                 if let Some(header) = msg.header {
-                    assert!(header.msg_id.clone().unwrap() == "AXSS-1544114083.761508");
+                    assert!(header.msg_id == Some("AXSS-1544114083.761508".into()));
                     assert!(header.msg_type == Some(ADD));
                 }
 
@@ -66,19 +131,18 @@ mod tests {
                             assert!(a.create_objs.len() == 1);
                             let createobj = &a.create_objs[0];
                             assert!(
-                                createobj.obj_path.clone().unwrap()
-                                    == "Device.LocalAgent.Controller."
+                                createobj.obj_path == Some("Device.LocalAgent.Controller.".into())
                             );
                             assert!(createobj.param_settings.len() == 2);
-                            let param1 = createobj.param_settings[0].clone();
-                            assert!(param1.param.clone().unwrap() == "Alias");
-                            assert!(param1.value.clone().unwrap() == "test");
-                            assert!(param1.required.clone().unwrap() == true);
+                            let param1 = &createobj.param_settings[0];
+                            assert!(param1.param == Some("Alias".into()));
+                            assert!(param1.value == Some("test".into()));
+                            assert!(param1.required == Some(true));
 
-                            let param2 = createobj.param_settings[1].clone();
-                            assert!(param2.param.clone().unwrap() == "EndpointID");
-                            assert!(param2.value.clone().unwrap() == "test");
-                            assert!(param2.required.clone().unwrap() == true);
+                            let param2 = &createobj.param_settings[1];
+                            assert!(param2.param == Some("EndpointID".into()));
+                            assert!(param2.value == Some("test".into()));
+                            assert!(param2.required == Some(true));
                         }
                     }
                 }
@@ -102,17 +166,14 @@ mod tests {
             0x57, 0x65, 0x62, 0x53, 0x6f, 0x63, 0x6b, 0x65, 0x74, 0x2e,
         ];
 
-        use rusp::usp::mod_Body::OneOfmsg_body::request;
-        use rusp::usp::mod_Header::MsgType::DELETE;
-        use rusp::usp::mod_Request::OneOfreq_type::delete;
-        use rusp::usp_record::mod_Record::OneOfrecord_type::no_session_context;
+        use rusp::usp::{mod_Header::MsgType::DELETE, mod_Request::OneOfreq_type::delete};
 
         let mut reader = BytesReader::from_bytes(&bytes);
         let record = Record::from_reader(&mut reader, &bytes).expect("Cannot read Record");
 
-        assert!(record.version.clone().unwrap() == "1.0");
-        assert!(record.to_id.clone().unwrap() == "proto::ax-usp-agent-nossl-websocket");
-        assert!(record.from_id.clone().unwrap() == "proto::ax-usp-controller-nossl");
+        assert!(record.version == Some("1.0".into()));
+        assert!(record.to_id == Some("proto::ax-usp-agent-nossl-websocket".into()));
+        assert!(record.from_id == Some("proto::ax-usp-controller-nossl".into()));
         assert!(record.payload_security == None);
         assert!(record.mac_signature == None);
         assert!(record.sender_cert == None);
@@ -129,7 +190,7 @@ mod tests {
                 assert!(msg.header.is_some());
 
                 if let Some(header) = msg.header {
-                    assert!(header.msg_id.clone().unwrap() == "AXSS-1544114102.668439");
+                    assert!(header.msg_id == Some("AXSS-1544114102.668439".into()));
                     assert!(header.msg_type == Some(DELETE));
                 }
 
@@ -144,9 +205,7 @@ mod tests {
                         if let delete(a) = req.req_type {
                             assert!(a.allow_partial == Some(true));
                             assert!(a.obj_paths.len() == 1);
-                            assert!(
-                                &a.obj_paths[0].clone() == "Device.LocalAgent.MTP.1.WebSocket."
-                            );
+                            assert!(&a.obj_paths[0] == "Device.LocalAgent.MTP.1.WebSocket.");
                         }
                     }
                 }
@@ -170,17 +229,14 @@ mod tests {
             0x62, 0x53, 0x6f, 0x63, 0x6b, 0x65, 0x74, 0x2e,
         ];
 
-        use rusp::usp::mod_Body::OneOfmsg_body::request;
-        use rusp::usp::mod_Header::MsgType::GET;
-        use rusp::usp::mod_Request::OneOfreq_type::get;
-        use rusp::usp_record::mod_Record::OneOfrecord_type::no_session_context;
+        use rusp::usp::{mod_Header::MsgType::GET, mod_Request::OneOfreq_type::get};
 
         let mut reader = BytesReader::from_bytes(&bytes);
         let record = Record::from_reader(&mut reader, &bytes).expect("Cannot read Record");
 
-        assert!(record.version.clone().unwrap() == "1.0");
-        assert!(record.to_id.clone().unwrap() == "proto::ax-usp-agent-nossl-websocket");
-        assert!(record.from_id.clone().unwrap() == "proto::ax-usp-controller-nossl");
+        assert!(record.version == Some("1.0".into()));
+        assert!(record.to_id == Some("proto::ax-usp-agent-nossl-websocket".into()));
+        assert!(record.from_id == Some("proto::ax-usp-controller-nossl".into()));
         assert!(record.payload_security == None);
         assert!(record.mac_signature == None);
         assert!(record.sender_cert == None);
@@ -197,7 +253,7 @@ mod tests {
                 assert!(msg.header.is_some());
 
                 if let Some(header) = msg.header {
-                    assert!(header.msg_id.clone().unwrap() == "AXSS-1544114045.442596");
+                    assert!(header.msg_id == Some("AXSS-1544114045.442596".into()));
                     assert!(header.msg_type == Some(GET));
                 }
 
@@ -211,9 +267,7 @@ mod tests {
 
                         if let get(a) = req.req_type {
                             assert!(a.param_paths.len() == 1);
-                            assert!(
-                                &a.param_paths[0].clone() == "Device.LocalAgent.MTP.1.WebSocket."
-                            );
+                            assert!(&a.param_paths[0] == "Device.LocalAgent.MTP.1.WebSocket.");
                         }
                     }
                 }
