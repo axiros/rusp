d95c48c61429b60246ad6b85124382459a175833
Daniel Egger <daniel.egger@axiros.com>
2024-01-17 13:42:59 +0100

    (Partial) Sneak preview of new builder API
    Signed-off-by: Daniel Egger <daniel.egger@axiros.com>

---
+++ src/usp_builder.rs
@@ -0,0 +1,45 @@
+#![allow(clippy::new_without_default)]
+
+mod add;
+mod delete;
+mod error;
+mod get;
+mod getinstances;
+mod getsupporteddm;
+mod getsupportedprotocol;
+mod msg;
+mod notify;
+mod operate;
+mod record;
+mod set;
+
+pub use add::{
+    AddBuilder, AddOperationStatus, AddRespBuilder, AddRespParameterError, CreateObjectBuilder,
+    CreatedObjectResultsBuilder,
+};
+pub use delete::{
+    DeleteBuilder, DeleteRespBuilder, DeleteRespOperationStatus, DeleteRespUnaffectedPathError,
+    DeletedObjectResultsBuilder,
+};
+pub use error::ErrorBuilder;
+pub use get::{
+    GetBuilder, GetRespBuilder, ReqPathResultBuilder as GetReqPathResultBuilder,
+    ResolvedPathResultBuilder,
+};
+pub use getinstances::{
+    CurrInstanceBuilder, GetInstancesBuilder, GetInstancesRespBuilder,
+    ReqPathResultBuilder as GetInstancesRespReqPathResultBuilder,
+};
+pub use getsupporteddm::{
+    GSDMCommandResult, GSDMEventResult, GSDMParamResult, GSDMReqObjectResultBuilder,
+    GSDMSupportedObjectResult, GetSupportedDMBuilder, GetSupportedDMRespBuilder,
+};
+pub use getsupportedprotocol::{GetSupportedProtocolBuilder, GetSupportedProtocolRespBuilder};
+pub use msg::MsgBuilder;
+pub use notify::NotifyBuilder;
+pub use operate::OperateBuilder;
+pub use record::RecordBuilder;
+pub use set::{
+    SetBuilder, SetOperationStatus, SetRespBuilder, SetRespParameterError, UpdateObjectBuilder,
+    UpdatedInstanceFailureBuilder, UpdatedObjectResultsBuilder,
+};
