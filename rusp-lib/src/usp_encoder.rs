fbfea9799f662ebff511302871a238d843013ca3
Daniel Egger <daniel.egger@axiros.com>
2025-05-20 11:37:56 +0200

    Add new `to_c_array_custom` encoders to allow specifying the variable name in the output
    Signed-off-by: Daniel Egger <daniel.egger@axiros.com>

--- rusp-lib/src/usp_encoder.rs
+++ rusp-lib/src/usp_encoder.rs
@@ -229,6 +229,32 @@ impl Record {
     /// assert_eq!(record.to_c_array().unwrap(), "unsigned int pb_len = 36;\nconst char pb[] = {\n  0x0a, 0x03, 0x31, 0x2e, 0x33, 0x12, 0x07, 0x64, /* __1.3__d */\n  0x6f, 0x63, 0x3a, 0x3a, 0x74, 0x6f, 0x1a, 0x09, /* oc__to__ */\n  0x64, 0x6f, 0x63, 0x3a, 0x3a, 0x66, 0x72, 0x6f, /* doc__fro */\n  0x6d, 0x52, 0x09, 0x08, 0x01, 0x12, 0x05, 0x74, /* mR_____t */\n  0x6f, 0x70, 0x69, 0x63,                         /* opic */\n};\n");
     /// ```
     pub fn to_c_array(&self) -> Result<String> {
+        self.to_c_array_custom("pb")
+    }
+
+    /// Render the `Record` into a raw C array representation with a custom name
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - A USP `Record` structure
+    /// * `name` - The variable name prefix used in the rendered output
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rusp_lib::usp_decoder::try_decode_record;
+    /// let record =
+    ///     try_decode_record(&[
+    ///         0x0a, 0x03, 0x31, 0x2e, 0x33, 0x12, 0x07, 0x64,
+    ///         0x6f, 0x63, 0x3a, 0x3a, 0x74, 0x6f, 0x1a, 0x09,
+    ///         0x64, 0x6f, 0x63, 0x3a, 0x3a, 0x66, 0x72, 0x6f,
+    ///         0x6d, 0x52, 0x09, 0x08, 0x01, 0x12, 0x05, 0x74,
+    ///         0x6f, 0x70, 0x69, 0x63,
+    ///     ]).unwrap();
+    /// assert_eq!(record.to_c_array_custom("rec").unwrap(), "unsigned int rec_len = 36;\nconst char rec[] = {\n  0x0a, 0x03, 0x31, 0x2e, 0x33, 0x12, 0x07, 0x64, /* __1.3__d */\n  0x6f, 0x63, 0x3a, 0x3a, 0x74, 0x6f, 0x1a, 0x09, /* oc__to__ */\n  0x64, 0x6f, 0x63, 0x3a, 0x3a, 0x66, 0x72, 0x6f, /* doc__fro */\n  0x6d, 0x52, 0x09, 0x08, 0x01, 0x12, 0x05, 0x74, /* mR_____t */\n  0x6f, 0x70, 0x69, 0x63,                         /* opic */\n};\n");
+    /// ```
+    pub fn to_c_array_custom(&self, name: &str) -> Result<String> {
+        const CHUNK_LEN: usize = 8;
         use std::fmt::Write as _;
 
         const fn check_printable(c: u8) -> bool {
@@ -243,9 +269,8 @@ impl Record {
         let data = self.to_vec()?;
         let mut out = String::new();
 
-        const CHUNK_LEN: usize = 8;
-        writeln!(out, "unsigned int pb_len = {};", data.len())?;
-        writeln!(out, "const char pb[] = {{")?;
+        writeln!(out, "unsigned int {name}_len = {};", data.len())?;
+        writeln!(out, "const char {name}[] = {{")?;
         for chunk in data.chunks(CHUNK_LEN) {
             write!(out, "  ")?;
             for i in chunk {
@@ -411,11 +436,11 @@ impl Msg {
         Ok(out)
     }
 
-    /// Render the `Record` into a raw C array representation
+    /// Render the `Msg` into a raw C array representation
     ///
     /// # Arguments
     ///
-    /// * `self` - A USP `Record` structure
+    /// * `self` - A USP `Msg` structure
     ///
     /// # Example
     ///
@@ -434,6 +459,34 @@ impl Msg {
     /// assert_eq!(msg.to_c_array().unwrap(), "unsigned int pb_len = 52;\nconst char pb[] = {\n  0x0a, 0x08, 0x0a, 0x04, 0x74, 0x65, 0x73, 0x74, /* ____test */\n  0x10, 0x03, 0x12, 0x28, 0x0a, 0x26, 0x42, 0x24, /* ___(__B_ */\n  0x0a, 0x05, 0x6e, 0x6f, 0x74, 0x69, 0x66, 0x10, /* __notif_ */\n  0x01, 0x42, 0x19, 0x0a, 0x06, 0x30, 0x30, 0x34, /* _B___004 */\n  0x34, 0x46, 0x46, 0x12, 0x03, 0x46, 0x6f, 0x6f, /* 4FF__Foo */\n  0x1a, 0x05, 0x30, 0x31, 0x32, 0x33, 0x34, 0x22, /* __01234\" */\n  0x03, 0x31, 0x2e, 0x33,                         /* _1.3 */\n};\n");
     /// ```
     pub fn to_c_array(&self) -> Result<String> {
+        self.to_c_array_custom("pb")
+    }
+
+    /// Render the `Msg` into a raw C array representation with a custom name
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - A USP `Msg` structure
+    /// * `name` - The variable name prefix used in the rendered output
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rusp_lib::usp_decoder::try_decode_msg;
+    /// let bytes = &[
+    ///         0x0a, 0x08, 0x0a, 0x04, 0x74, 0x65, 0x73, 0x74,
+    ///         0x10, 0x03, 0x12, 0x28, 0x0a, 0x26, 0x42, 0x24,
+    ///         0x0a, 0x05, 0x6e, 0x6f, 0x74, 0x69, 0x66, 0x10,
+    ///         0x01, 0x42, 0x19, 0x0a, 0x06, 0x30, 0x30, 0x34,
+    ///         0x34, 0x46, 0x46, 0x12, 0x03, 0x46, 0x6f, 0x6f,
+    ///         0x1a, 0x05, 0x30, 0x31, 0x32, 0x33, 0x34, 0x22,
+    ///         0x03, 0x31, 0x2e, 0x33,
+    ///     ];
+    /// let msg = try_decode_msg(bytes).unwrap();
+    /// assert_eq!(msg.to_c_array_custom("msg").unwrap(), "unsigned int msg_len = 52;\nconst char msg[] = {\n  0x0a, 0x08, 0x0a, 0x04, 0x74, 0x65, 0x73, 0x74, /* ____test */\n  0x10, 0x03, 0x12, 0x28, 0x0a, 0x26, 0x42, 0x24, /* ___(__B_ */\n  0x0a, 0x05, 0x6e, 0x6f, 0x74, 0x69, 0x66, 0x10, /* __notif_ */\n  0x01, 0x42, 0x19, 0x0a, 0x06, 0x30, 0x30, 0x34, /* _B___004 */\n  0x34, 0x46, 0x46, 0x12, 0x03, 0x46, 0x6f, 0x6f, /* 4FF__Foo */\n  0x1a, 0x05, 0x30, 0x31, 0x32, 0x33, 0x34, 0x22, /* __01234\" */\n  0x03, 0x31, 0x2e, 0x33,                         /* _1.3 */\n};\n");
+    /// ```
+    pub fn to_c_array_custom(&self, name: &str) -> Result<String> {
+        const CHUNK_LEN: usize = 8;
         use std::fmt::Write as _;
 
         const fn check_printable(c: u8) -> bool {
@@ -448,9 +501,8 @@ impl Msg {
         let data = self.to_vec()?;
         let mut out = String::new();
 
-        const CHUNK_LEN: usize = 8;
-        writeln!(out, "unsigned int pb_len = {};", data.len())?;
-        writeln!(out, "const char pb[] = {{")?;
+        writeln!(out, "unsigned int {name}_len = {};", data.len())?;
+        writeln!(out, "const char {name}[] = {{")?;
         for chunk in data.chunks(CHUNK_LEN) {
             write!(out, "  ")?;
             for i in chunk {
